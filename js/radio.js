c8832052281c63d245c93b7fcd71b2d749bd08a4
Franco Luciano Leonardi <francoreload@gmail.com>
2020-05-04 20:23:23 -0300

    fix styling

--- js/radio.js
+++ js/radio.js
@@ -51,11 +51,11 @@ function saveMessage(messageText) {
 
 // Loads chat messages history and listens for upcoming ones.
 function loadMessages() {
-    // Create the query to load the last 12 messages and listen for new ones.
+    // Create the query to load the last 10 messages and listen for new ones.
     var query = firebase.firestore()
         .collection('messages')
         .orderBy('timestamp', 'desc')
-        .limit(12);
+        .limit(10);
 
     // Start listening to the query.
     query.onSnapshot(function(snapshot) {
@@ -76,8 +76,9 @@ function onMessageFormSubmit(e) {
     e.preventDefault();
     // Check that the user entered a message and is signed in.
     if (messageInputElement.value && checkSignedInWithMessage()) {
-        saveMessage(messageInputElement.value).then(function() {
-            messageInputElement.value = "";
+        let message = messageInputElement.value;
+        messageInputElement.value = "";
+        saveMessage(message).then(function() {
             // Clear message text field and re-enable the SEND button.
             toggleButton();
         });
@@ -88,10 +89,22 @@ function onMessageFormSubmit(e) {
 function authStateObserver(user) {
     if (user) { // User is signed in!
         signOutButtonElement.removeAttribute('hidden');
+
+        submitButtonElement.removeAttribute('hidden');
+
+        messageInputElement.setAttribute('placeholder', 'Escribe un mensaje...');
+        messageInputElement.removeAttribute('disabled');
+
         // Hide sign-in button.
         signInButtonElement.setAttribute('hidden', 'true');
     } else { // User is signed out!
         signOutButtonElement.setAttribute('hidden', 'true');
+
+        submitButtonElement.setAttribute('hidden', 'true');
+
+        messageInputElement.setAttribute('placeholder', 'Inicia sesion para enviar mensajes');
+        messageInputElement.setAttribute('disabled', 'true');
+
         // Show sign-in button.
         signInButtonElement.removeAttribute('hidden');
     }
@@ -104,7 +117,6 @@ function checkSignedInWithMessage() {
         return true;
     }
 
-    $('.toast').toast('show');
     return false;
 }
 
@@ -227,8 +239,14 @@ function checkSetup() {
     }
 }
 
-// Checks that Firebase has been imported.
-checkSetup();
+function radioError() {
+    radioElement.innerHTML = "<h4>Volve el sabado a las 20:00hs</h4>";
+    radioElement.removeAttribute('hidden');
+}
+
+function radioSuccess() {
+    radioElement.removeAttribute('hidden');
+}
 
 // Shortcuts to DOM Elements.
 var messageListElement = document.getElementById('messages-content');
@@ -238,6 +256,7 @@ var submitButtonElement = document.getElementById('submit');
 var signInSnackbarElement = document.getElementById('must-signin-snackbar');
 var signInButtonElement = document.getElementById('sign-in');
 var signOutButtonElement = document.getElementById('sign-out');
+var radioElement = document.getElementById('radio-player');
 
 // Saves message on form submit.
 messageFormElement.addEventListener('submit', onMessageFormSubmit);
@@ -247,6 +266,9 @@ signInButtonElement.addEventListener('click', signIn);
 // Toggle for the button.
 messageInputElement.addEventListener('keyup', toggleButton);
 messageInputElement.addEventListener('change', toggleButton);
+
+// Checks that Firebase has been imported.
+checkSetup();
 
 // initialize Firebase
 initFirebaseAuth();
